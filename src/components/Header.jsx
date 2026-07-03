e31cadcfa88abfefa24233ab6ef930429a8d9689
DanTheMan95 <81121522+Danx3mer@users.noreply.github.com>
2026-07-02 22:43:13 -0400

    fix: properly display Header on the Home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../css/Header.css"
 export default function Header({user, type}) {
 	return (
 		<div id="Header">
-		{type in ["Admin", "Teacher", "Student"] && <h1>{`Welcome, ${type} ${user}!`}</h1>}
+		{["Admin", "Teacher", "Student"].includes(type) && <h1>{`Welcome, ${type} ${user}!`}</h1>}
 		</div>
 	);
 }
