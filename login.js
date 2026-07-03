642a290dd26b679aebd35351ebc64406e5564a8c
DanTheMan95 <81121522+Danx3mer@users.noreply.github.com>
2026-07-03 15:14:18 -0400

    clean: remove files from old release

--- login.js
+++
@@ -1,9 +0,0 @@
-const queryString = window.location.search;
-			
-const urlParams = new URLSearchParams(queryString);
-
-const user = urlParams.get('login');
-
-if(user!=null) {
-	document.getElementById("successIndicator").hidden=false;
-}
